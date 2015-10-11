4e7c7d20b9434902bb5202284c0f69640ddc1578
Ivan Kravarscan <subchannel13@gmail.com>
2015-10-11 15:32:05 +0200

    Imported item subtypes

---
+++ MetaData/ItemStereotype.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Merchant_RPG_build.MetaData
+{
+	class ItemStereotype
+	{
+		public readonly double AttackBase;
+		public readonly double MagicAttackBase;
+		public readonly double AccuracyBase;
+		public readonly double CriticalRateBase;
+
+		public readonly double DefenseBase;
+		public readonly double MagicDefenseBase;
+
+		public readonly double AttackPerLevel;
+		public readonly double MagicAttackPerLevel;
+		public readonly double AccuracyPerLevel;
+		public readonly double CriticalRatePerLevel;
+
+		public readonly double DefensePerLevel;
+		public readonly double MagicDefensePerLevel;
+
+		public ItemStereotype(double attackBase, double magicAttackBase, double accuracyBase, double criticalRateBase,
+			double defenseBase, double magicDefenseBase, double attackPerLevel, double magicAttackPerLevel,
+			double accuracyPerLevel, double criticalRatePerLevel, double defensePerLevel, double magicDefensePerLevel)
+		{
+			this.AccuracyBase = accuracyBase;
+			this.AccuracyPerLevel = accuracyPerLevel;
+			this.AttackBase = attackBase;
+			this.AttackPerLevel = attackPerLevel;
+			this.CriticalRateBase = criticalRateBase;
+			this.CriticalRatePerLevel = criticalRatePerLevel;
+			this.DefenseBase = defenseBase;
+			this.DefensePerLevel = defensePerLevel;
+			this.MagicAttackBase = magicAttackBase;
+			this.MagicAttackPerLevel = magicAttackPerLevel;
+			this.MagicDefenseBase = magicDefenseBase;
+			this.MagicDefensePerLevel = magicDefensePerLevel;
+		}
+	}
+}
--- MetaData/Library.cs
+++ MetaData/Library.cs
@@ -32,6 +32,100 @@ namespace Merchant_RPG_build.MetaData
 			         7, 0.5, 0.5, 0.3, 0, 0.4, 0.4)
 		};
 		
+		public static ItemStereotype DaggerStereotype = new ItemStereotype(
+							16, 4, 20, 5, 0, 0, 
+							0.75, 0, 1.25, 5, 0, 0);
+		public static ItemStereotype KnifeStereotype = new ItemStereotype(
+							16, 4, 20, 5, 0, 0, 
+							0.5, 0, 1.5, 5, 0, 0);
+		public static ItemStereotype SwordStereotype = new ItemStereotype(
+							18, 4, 18, 1, 0, 0, 
+							1, 0, 1, 1, 0, 0);
+		public static ItemStereotype LongswordStereotype = new ItemStereotype(
+							18, 4, 18, 1, 0, 0, 
+							1.25, 0, 0.75, 1, 0, 0);
+		public static ItemStereotype HatchetStereotype = new ItemStereotype(
+							21, 4, 13, 1, 0, 0, 
+							1.5, 0, 0.5, 1, 0, 0);
+		public static ItemStereotype AxeStereotype = new ItemStereotype(
+							21, 4, 13, 1, 0, 0, 
+							1.75, 0, 0.25, 1, 0, 0);
+		public static ItemStereotype WandStereotype = new ItemStereotype(
+							4, 19, 17, 1, 0, 0, 
+							0, 1, 1, 1, 0, 0);
+		public static ItemStereotype MysticWandStereotype = new ItemStereotype(
+							4, 19, 17, 1, 0, 0, 
+							0, 1.25, 0.75, 1, 0, 0);
+		public static ItemStereotype ClubStereotype = new ItemStereotype(
+							10, 16, 12, 1, 0, 0, 
+							0.5, 1.25, 0.25, 1, 0, 0);
+		public static ItemStereotype CudgelStereotype = new ItemStereotype(
+							13, 13, 12, 1, 0, 0, 
+							0.875, 0.875, 0.25, 1, 0, 0);
+		public static ItemStereotype StaveStereotype = new ItemStereotype(
+							4, 22, 12, 1, 0, 0, 
+							0, 1.5, 0.5, 1, 0, 0);
+		public static ItemStereotype MysticStaveStereotype = new ItemStereotype(
+							4, 22, 12, 1, 0, 0, 
+							0, 1.75, 0.25, 1, 0, 0);
+		public static ItemStereotype RodStereotype = new ItemStereotype(
+							8, 18, 12, 1, 0, 0, 
+							0.5, 1, 0.5, 1, 0, 0);
+		public static ItemStereotype PoleStereotype = new ItemStereotype(
+							8, 15, 17, 1, 0, 0, 
+							0.5, 0.75, 0.75, 1, 0, 0);
+		public static ItemStereotype SpellswordStereotype = new ItemStereotype(
+							16, 6, 18, 1, 0, 0, 
+							0.75, 0.25, 1, 1, 0, 0);
+		public static ItemStereotype HelmStereotype = new ItemStereotype(
+							0, 0, 0, 0, 8, 4, 
+							0, 0, 0, 0, 0.8, 0.4);
+		public static ItemStereotype GreatHelmStereotype = new ItemStereotype(
+							0, 0, 0, 0, 10, 2, 
+							0, 0, 0, 0, 1, 0.2);
+		public static ItemStereotype HoodStereotype = new ItemStereotype(
+							0, 0, 0, 0, 6, 6, 
+							0, 0, 0, 0, 0.6, 0.6);
+		public static ItemStereotype HatStereotype = new ItemStereotype(
+							0, 0, 0, 0, 4, 8, 
+							0, 0, 0, 0, 0.4, 0.8);
+		public static ItemStereotype ChainmailStereotype = new ItemStereotype(
+							0, 0, 0, 0, 12, 4, 
+							0, 0, 0, 0, 1.4, 0.4);
+		public static ItemStereotype PlatemailStereotype = new ItemStereotype(
+							0, 0, 0, 0, 14, 2, 
+							0, 0, 0, 0, 1.6, 0.2);
+		public static ItemStereotype TunicStereotype = new ItemStereotype(
+							0, 0, 0, 0, 8, 8, 
+							0, 0, 0, 0, 0.9, 0.9);
+		public static ItemStereotype RobeStereotype = new ItemStereotype(
+							0, 0, 0, 0, 4, 12, 
+							0, 0, 0, 0, 0.4, 1.4);
+		public static ItemStereotype BracersStereotype = new ItemStereotype(
+							0, 0, 0, 0, 6, 2, 
+							0, 0, 0, 0, 0.4, 0.2);
+		public static ItemStereotype GauntletsStereotype = new ItemStereotype(
+							0, 0, 0, 0, 7, 1, 
+							0, 0, 0, 0, 0.5, 0.1);
+		public static ItemStereotype GlovesStereotype = new ItemStereotype(
+							0, 0, 0, 0, 4, 4, 
+							0, 0, 0, 0, 0.3, 0.3);
+		public static ItemStereotype MittsStereotype = new ItemStereotype(
+							0, 0, 0, 0, 2, 6, 
+							0, 0, 0, 0, 0.2, 0.4);
+		public static ItemStereotype GreavesStereotype = new ItemStereotype(
+							0, 0, 0, 0, 6, 2, 
+							0, 0, 0, 0, 0.4, 0.2);
+		public static ItemStereotype SabatonsStereotype = new ItemStereotype(
+							0, 0, 0, 0, 7, 1, 
+							0, 0, 0, 0, 0.5, 0.1);
+		public static ItemStereotype BootsStereotype = new ItemStereotype(
+							0, 0, 0, 0, 4, 4, 
+							0, 0, 0, 0, 0.3, 0.3);
+		public static ItemStereotype CrakowsStereotype = new ItemStereotype(
+							0, 0, 0, 0, 2, 6, 
+							0, 0, 0, 0, 0.2, 0.4);
+
 		public static Item[] Blacksmith = new Item[] {
 			new Item("Beast Hatchet", 37, ItemSlot.Weapon, attack: 81, magicAttack: 5, accuracy: 34, criticalRate: 2, dexterity: 3, hp: 25),
 			new Item("Beast Sword", 36, ItemSlot.Weapon, attack: 65, magicAttack: 5, accuracy: 65, criticalRate: 2, dexterity: 3, hp: 25),
